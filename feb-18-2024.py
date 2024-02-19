298da4670b2e5dff8ff10a7acf65a722685e8247
root <root@Jesses-Laptop>
2024-02-18 22:47:17 -0600

    finished problem for feb 18

---
+++ feb-18-2024.py
@@ -0,0 +1,26 @@
+from collections import deque
+class LRU:
+    def __init__(self, size):
+        self.size = size
+        self.current_keys = deque()
+        self.cache = {}
+    
+    def set(self, key, value):
+        if len(self.current_keys) == self.size:
+            del self.cache[self.current_keys[-1]] 
+            self.current_keys.pop()
+        self.current_keys.appendleft(key)
+        self.cache[key] = value
+    
+    def get(self, key):
+        if key in self.cache:
+            return self.cache[key]
+
+        return "Not Found"
+
+obj1 = LRU(2)
+obj1.set('pos1', 12)
+obj1.set('pos2', 13)
+obj1.set('pos3', 14)
+print(obj1.get('pos2'))
+
